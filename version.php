5939083a58e2ee09198f83c39ccfda5d4666fe5f
jamesorlakin <jamesorlakin@gmail.com>
2014-08-04 13:16:18 +0100

    Removed code that updated the configuration after an old update (no longer needed).

--- version.php
+++ version.php
@@ -9,6 +9,7 @@ $version = "$versionnum by Jamster Technologies";
 if (!isset($mdb)) {
 require('settings.php');
 }
+
 if ($updatecheck == "y") {
 $fromweb = @file_get_contents("http://jameslakin.co.uk/stockwizard/version.txt?rnd=" . rand(1,1000));
 if (empty($fromweb)) {
@@ -24,65 +25,13 @@ $versionupdate = false;
 }
 }
 
-$setfile = fopen("settings.php", 'r');
 if (!file_exists("settings.php.bak")) {
+$setfile = fopen("settings.php", 'r');
 $setbackup = fopen("settings.php.bak", 'w');
 fwrite($setbackup, fread($setfile, 700));
 fclose($setbackup);
-}
-// UPDATING CONFIG SYSTEM IF NOT DONE ALREADY
-if (is_string(strstr(fread($setfile, 280), '$mpss ='))) {
-if (!isset($mpss)) {
-require('settings.php');
-}
-if (!is_dir('config')) {
-mkdir('config');
-}
-// COPY CURRENT SETTINGS INTO DIFFERENT FILES FOR EACH
-$configfile = fopen('config/db.php', 'w');
-fwrite($configfile, '<?php $mdb="' . $mdb . '"; ?>');
-fclose($configfile);
-
-$configfile = fopen('config/usr.php', 'w');
-fwrite($configfile, '<?php $musr="' . $musr . '"; ?>');
-fclose($configfile);
-
-$configfile = fopen('config/pss.php', 'w');
-fwrite($configfile, '<?php $mpss="' . $mpss . '"; ?>');
-fclose($configfile);
-
-$configfile = fopen('config/host.php', 'w');
-fwrite($configfile, '<?php $mhost="' . $mhost . '"; ?>');
-fclose($configfile);
-
-$configfile = fopen('config/salt.php', 'w');
-fwrite($configfile, '<?php $salt="' . $salt . '"; ?>');
-fclose($configfile);
-
-$configfile = fopen('config/allowauto.php', 'w');
-fwrite($configfile, '<?php $allowauto="' . $allowauto . '"; ?>');
-fclose($configfile);
-
-$configfile = fopen('config/autoneedsalt.php', 'w');
-fwrite($configfile, '<?php $autoneedsalt="' . $autoneedsalt . '"; ?>');
-fclose($configfile);
-
-$configfile = fopen('config/businessname.php', 'w');
-fwrite($configfile, '<?php $businessname="' . $businessname . '"; ?>');
-fclose($configfile);
-
-// DELETE CURRENT SETTINGS FILE
-@unlink('settings.php');
-// MAKE SETTINGS FILE INCLUDE FILES
-$configfile = fopen('settings.php', 'w');
-fwrite($configfile, '<?php foreach (glob("config/*.php") as $filename) { include($filename); } include("version.php"); ?>');
-fclose($configfile);
-// REPORT THAT CONFIG HAS BEEN UPDATED
-$report = "Config system updated, all configuration files are in the 'config' directory";
-}
-
-
 fclose($setfile);
+}
 
 // CHECK ALL FILES EXIST
 $checkfile = 'allowauto.php';
@@ -105,7 +54,7 @@ $checkfile = 'businessname.php';
 if (!file_exists('config/' . $checkfile)) {
 $report = "Config files error, default setting applied for '$checkfile'!";
 $checkfilehandle = fopen('config/' . $checkfile, 'w');
-fwrite($checkfilehandle, '<?php $businessname="Change this business name in config/businessname.php"; ?>');
+fwrite($checkfilehandle, '<?php $businessname="Change this business name in config/businessname.php or using the configuration utility."; ?>');
 fclose($checkfilehandle);
 }
 
@@ -161,7 +110,7 @@ $checkfile = 'widgetenabled.php';
 if (!file_exists('config/' . $checkfile)) {
 $report = "Config files error, default setting applied for '$checkfile'!";
 $checkfilehandle = fopen('config/' . $checkfile, 'w');
-fwrite($checkfilehandle, '<?php $widgetenabled="y"; ?>');
+fwrite($checkfilehandle, '<?php $widgetenabled="n"; ?>');
 fclose($checkfilehandle);
 }
 
